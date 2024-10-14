e7b5f2147a39534bee739a1f47c4136b0a4eb075
nazarkdl <147524007+nazarkdl@users.noreply.github.com>
2024-10-14 15:27:32 +0200

    TEST: Testing Pull Request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 'use client'
 import React from 'react';
 
-const Home: React.FC = () => {
-  return <div>Welcome to the R&D Project!</div>;
+const Home: React.FC = () => { 
+  return (
+    <div>
+      <h1>Welcome to the R&D Project!</h1>
+      <p>This is a simple page rendered on the client side.</p>
+    </div>
+  );
 };
 
 export default Home;
