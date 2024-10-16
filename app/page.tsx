7387e25ac4f1a0499ea8155296d26f665543134a
user <georgeadelin.aniei@kuleuven.student.be>
2024-10-16 12:32:27 +0200

    FIXED ISSUE: Tailwind is now working.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React from 'react';
 
+
 const Home: React.FC = () => { 
   return (
     <div>
-      <h1>Welcome to the R&D Project!</h1>
+      <h1 className="text-3xl text-red bg-red-900">Welcome to the R&D Project!</h1>
       <p>This is a simple page rendered on the client side.</p>
     </div>
   );
