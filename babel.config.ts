8057579c2c6034544a0ec10b5d55f120855e58e7
jaskaransingh sidhu <jaskaransingh.sidhu@student.kuleuven.be>
2024-10-25 01:47:45 +0200

    babel config added

---
+++ babel.config.ts
@@ -0,0 +1,9 @@
+// babel.config.js
+module.exports = {
+    presets: [
+      '@babel/preset-env',       // Transpile ES6+ down to ES5
+      '@babel/preset-react',     // Transform JSX
+      '@babel/preset-typescript', // Support TypeScript
+    ],
+  };
+  
