bb4b7b7e20aaeb022df39f75238ed81ae05fb6eb
jaskaransingh sidhu <jaskaransingh.sidhu@student.kuleuven.be>
2024-11-04 14:06:29 +0100

    babel and jest config modified

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env',      // Transpile modern JavaScript
+      '@babel/preset-react',    // Transform JSX
+      '@babel/preset-typescript' // Handle TypeScript
+    ]
+  };
+  
--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest', // Use ts-jest preset to handle TypeScript
-  testEnvironment: 'jsdom', // Use jsdom for testing React components
+  preset: 'ts-jest', 
+  testEnvironment: 'jsdom', 
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
-    '\\.(css|scss|sass)$': 'identity-obj-proxy', // Mocks CSS imports
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
   },
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript files
-    '^.+\\.jsx?$': 'babel-jest',   // Transform JavaScript and JSX files
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    '/node_modules/', // Ensure to transform node_modules if necessary
+    '/node_modules/',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
