378433871679e57be2639c37d83b74b8926426e1
Ezzi Belkaid <ezzi.belkaid@student.kuleuven.be>
2024-11-15 15:20:40 +0100

    Changing the environmental variables in index.js to directly access the URL and API keys

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const { createClient } = require('@supabase/supabase-js'); // Import Supabase client
 const app = express();
 // Determine if we're in production or development
-const isProduction = process.env.NODE_ENV === 'production';
-const supabaseUrl = isProduction ? process.env.SUPABASE_URL_PROD : process.env.SUPABASE_URL_DEV;
-const supabaseKey = isProduction ? process.env.SUPABASE_KEY_PROD : process.env.SUPABASE_KEY_DEV;
+const isProduction  = false;
+const supabaseUrl = isProduction ? "https://omezdwvclavebxjljgeb.supabase.co" : "https://uqcrymzvamkqgrpribjf.supabase.co";
+const supabaseKey = isProduction ? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tZXpkd3ZjbGF2ZWJ4amxqZ2ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA1NTUyMjYsImV4cCI6MjA0NjEzMTIyNn0.teFBO9QdY4kxdIuXcqoIALsNLRMHvB77yuX6wHCbaA4" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxY3J5bXp2YW1rcWdycHJpYmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMTM2MTAsImV4cCI6MjA0NTc4OTYxMH0.Br46z5HHUTT3m7U1w9tbnF1VA4KhnauoExEhUNMPVZo";
 // Initialize the Supabase client
 const supabase = createClient(supabaseUrl, supabaseKey);
 app.get('/', (req, res) => {
