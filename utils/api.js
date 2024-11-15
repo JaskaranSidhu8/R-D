c8bd93f73fe8069f905e703937f667622bddf5a0
Ezzi Belkaid <ezzi.belkaid@student.kuleuven.be>
2024-11-15 11:42:36 +0100

    Connecting frontend with supabase API by fetching test images from supabase storage bucket in dev

---
+++ utils/api.js
@@ -0,0 +1,26 @@
+import { supabase } from './supabaseClient';
+
+export const fetchQuizImages = async () => {
+  try {
+    const { data, error } = await supabase.storage.from('quiz-images') // Replace with your bucket name
+      .list();
+
+    if (error) {
+      throw error;
+    }
+
+    // Map the fetched data to include both name and URL
+    console.log('Data from Supabase:', data); // Add this line
+    const images = data.map((file) => ({
+      name: file.name,
+      url: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/quiz-images/${file.name}`,
+    }));
+
+    return images;
+  } catch (error) {
+    console.error('Error fetching quiz images:', error.message);
+    return [];
+  }
+
+
+};
