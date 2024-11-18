372d30e27b40293a1337ad93a4722048c3de1a39
mFadi <99912690+mFadi98@users.noreply.github.com>
2024-11-18 19:09:02 +0100

    added conversation animation to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ConversationSection from "@/components/landingpage/ConversationSection";
 import Header from "@/components/landingpage/Header";
 import React from "react";
 const Home: React.FC = () => {
   return (
     <main>
       <Header />
+      <ConversationSection />
     </main>
   );
 };
