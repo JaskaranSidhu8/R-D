08a593bb6c8f7e31ed100f6d5d3225b4e072af8e
mFadi <99912690+mFadi98@users.noreply.github.com>
2024-11-25 00:44:37 +0100

    added content for result page , carousel for restaurant images.

---
+++ app/(NoLogoPages)/Result/page.tsx
@@ -0,0 +1,14 @@
+import ResultInfo from "@/components/Result/ResultInfo";
+import React from "react";
+
+type Props = {};
+
+const Result = (props: Props) => {
+  return (
+    <div>
+      <ResultInfo />
+    </div>
+  );
+};
+
+export default Result;
