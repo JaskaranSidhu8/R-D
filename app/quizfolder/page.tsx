31031b7dd0f3729f913dfa5286a4d84044d66f3b
nazarkdl <147524007+nazarkdl@users.noreply.github.com>
2024-11-25 21:21:37 +0100

    Supabase Fix 2

--- app/quizfolder/page.tsx
+++ app/quizfolder/page.tsx
@@ -1,33 +1,33 @@
-// "use client"; // Add this at the top
-// import React, { useEffect, useState } from "react";
+"use client"; // Add this at the top
+import React from "react";
 // import { fetchQuizImages } from "../../utils/api"; // Adjust the path as necessary
-// // Define the Image interface
+// Define the Image interface
 // interface Image {
 //   name: string;
 //   url: string;
 // }
-// const Quiz = () => {
-//   const [images, setImages] = useState<Image[]>([]);
-//   useEffect(() => {
-//     const loadImages = async () => {
-//       const fetchedImages = await fetchQuizImages();
-//       console.log("Fetched Images:", fetchedImages); // Log the images
-//       if (fetchedImages) {
-//         setImages(fetchedImages);
-//       }
-//     };
+const Quiz = () => {
+  // const [images, setImages] = useState<Image[]>([]);
+  // useEffect(() => {
+  //   const loadImages = async () => {
+  //     const fetchedImages = await fetchQuizImages();
+  //     console.log("Fetched Images:", fetchedImages); // Log the images
+  //     if (fetchedImages) {
+  //       setImages(fetchedImages);
+  //     }
+  //   };
 
-//     loadImages();
-//   }, []);
-//   return (
-//     <div>
-//       <h1>Quiz Images</h1>
-//       <div>
-//         {images.map((image) => (
-//           <img key={image.name} src={image.url} alt={image.name} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-// export default Quiz;
+  //   loadImages();
+  // }, []);
+  return (
+    <div>
+      <h1>Quiz Images</h1>
+      <div>
+        {/* {images.map((image) => (
+          <img key={image.name} src={image.url} alt={image.name} />
+        ))} */}
+      </div>
+    </div>
+  );
+};
+export default Quiz;
