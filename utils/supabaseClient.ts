1698b8852e913d57c32475d31812c4678c4b3f51
nazarkdl <147524007+nazarkdl@users.noreply.github.com>
2024-11-28 17:35:54 +0100

    Test Supabase

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,14 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Use environment variables for the Supabase URL and Key
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_DEV!;
-// const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY_DEV!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_DEV!;
+const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY_DEV!;
 
-const supabaseUrl = "https://uqcrymzvamkqgrpribjf.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxY3J5bXp2YW1rcWdycHJpYmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMTM2MTAsImV4cCI6MjA0NTc4OTYxMH0.Br46z5HHUTT3m7U1w9tbnF1VA4KhnauoExEhUNMPVZo";
+if (
+  !process.env.NEXT_PUBLIC_SUPABASE_URL_DEV ||
+  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_DEV
+) {
+  throw new Error("Supabase environment variables are not set!");
+}
 
 // Initialize Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
+// const supabaseUrl = "https://uqcrymzvamkqgrpribjf.supabase.co";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxY3J5bXp2YW1rcWdycHJpYmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMTM2MTAsImV4cCI6MjA0NTc4OTYxMH0.Br46z5HHUTT3m7U1w9tbnF1VA4KhnauoExEhUNMPVZo";
