2f521b31c4615466de59ac9e1a9835c7314ac00e
nazarkdl <147524007+nazarkdl@users.noreply.github.com>
2024-11-28 17:07:16 +0100

    Supabase Vercel Deployment Test

--- app/quizfolder/page.tsx
+++ app/quizfolder/page.tsx
@@ -1,39 +1,39 @@
 "use client"; // Add this at the top
-import React from "react";
-import { supabase } from "../../utils/supabaseClient.ts";
-// import { fetchQuizImages } from "../../utils/api"; // Adjust the path as necessary
-// Define the Image interface
-// interface Image {
-//   name: string;
-//   url: string;
-// }
+// import React from "react";
+import React, { useEffect, useState } from "react";
+// import supabase from "../../utils/supabaseClient";
+import { fetchQuizImages } from "../../utils/api"; // Adjust the path as necessary
+// // Define the Image interface
+interface Image {
+  name: string;
+  url: string;
+}
 const Quiz = () => {
-  // const [images, setImages] = useState<Image[]>([]);
-  // useEffect(() => {
-  //   const loadImages = async () => {
-  //     const fetchedImages = await fetchQuizImages();
-  //     console.log("Fetched Images:", fetchedImages); // Log the images
-  //     if (fetchedImages) {
-  //       setImages(fetchedImages);
-  //     }
-  //   };
+  const [images, setImages] = useState<Image[]>([]);
+  useEffect(() => {
+    const loadImages = async () => {
+      const fetchedImages = await fetchQuizImages();
+      console.log("Fetched Images:", fetchedImages); // Log the images
+      if (fetchedImages) {
+        setImages(fetchedImages);
+      }
+    };
 
-  //   loadImages();
-  // }, []);
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
+    loadImages();
+  }, []);
 
   return (
     <div>
       <h1>Quiz Images</h1>
       <div>
-        {images.map((image) => (
-          <img key={image.name} src={image.url} alt={image.name} />
-        ))}
+        <ul>
+          {images.map((image) => (
+            <img key={image.name} src={image.url} alt={image.name} />
+          ))}
+        </ul>
       </div>
     </div>
   );
 };
+
 export default Quiz;
--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Use environment variables for the Supabase URL and Key
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL_DEV!;
-const supabaseKey = process.env.REACT_APP_ANON_KEY_DEV!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_DEV!;
+const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY_DEV!;
 
 // Initialize Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
