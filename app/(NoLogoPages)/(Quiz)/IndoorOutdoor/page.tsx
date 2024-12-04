d80db57d4d8abd9644a1fbf088aadb387d19afea
Dele Ayeni <oladelejoshuaagboola.ayeni@student.kuleuven.be>
2024-12-04 15:53:13 +0100

    added a smooth transition between cuisine and indooroutdoor pages

--- app/(NoLogoPages)/(Quiz)/IndoorOutdoor/page.tsx
+++ app/(NoLogoPages)/(Quiz)/IndoorOutdoor/page.tsx
@@ -3,14 +3,17 @@
 import React from "react";
 import QuizHeader from "@/components/QuizPages/QuizHeader";
 import IndoorOutdoorForm from "@/components/QuizPages/IndoorOutdoorForm";
+import { QuizTransition } from "@/components/QuizPages/QuizTransition";
 
 const IndoorOutdoor = () => {
   return (
     <div>
       <QuizHeader progress={20} />
-      <div className="flex justify-center">
+      <QuizTransition>
+        {" "}
         <IndoorOutdoorForm />
-      </div>
+      </QuizTransition>
+      {/* <div className="flex justify-center"></div> */}
     </div>
   );
 };
